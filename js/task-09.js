86e28faaa903ca76cb17f3be826e3e7f5f676c4c
MarynaIlchenko <debovamail@gmail.com>
2022-01-15 13:46:48 +0200

    9 task_1

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,9 @@
+const colorRef = document.querySelector('.widget');
+const spanRef = document.querySelector('.color');
+colorRef.addEventListener('click', onButtonClick);
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+
