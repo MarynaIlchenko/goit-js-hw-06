411931b5814f4a41623efa09a5dd7358250954a7
MarynaIlchenko <debovamail@gmail.com>
2022-01-16 10:53:21 +0200

    task 1 update

--- js/task-01.js
+++ js/task-01.js
@@ -1,13 +1,9 @@
-const elemRef = document.querySelector('ul#categories');
-const countNumberOfCategories = elemRef.children.length;
+const elemRef = document.querySelectorAll('.item');
+const countNumberOfCategories = elemRef.length;
 console.log(` Number of categories: ${countNumberOfCategories}`);
 
-const liItemRef = elemRef.children;
-for (let i = 0; i < liItemRef.length; i += 1) {
-    const numberOfLi = liItemRef[i].children;
-
-    const textContent = numberOfLi[0].textContent;
-    console.log("Category: ", textContent);
-    const countNumberOfLi = numberOfLi[1].children.length;
-    console.log("Elements: ", countNumberOfLi);
-}
+elemRef.forEach(element => {
+    console.log(`Category: ${element.firstElementChild.textContent}`);
+    console.log(`Category: ${element.lastElementChild.children.length}`);
+});
+ 
